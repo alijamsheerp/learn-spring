fdc573fdd909b849fe10432e3948899cda42f634
jamshee <jamshee@gmail.com>
2020-10-31 11:37:51 -0400

    # WARNING: head commit changed in the meantime
    intial commit

---
+++ jam-spring-boot/src/main/java/com/jam/springboot/JamSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jam.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamSpringBootApplication.class, args);
+	}
+
+}
---
+++ jam-spring-boot/src/test/java/com/jam/springboot/JamSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
