369bde4c97c72360275ac3d54f5b028a8dfa1814
jamshee <jamshee@gmail.com>
2020-10-31 14:21:47 -0400

    jam-spring-boot Spring Environment object

--- jam-spring-boot/src/main/java/com/jam/springboot/controllers/RootController.java
+++ jam-spring-boot/src/main/java/com/jam/springboot/controllers/RootController.java
@@ -1,16 +1,24 @@
 package com.jam.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/root/v1")
+@PropertySource("some-prop-file.properties")
 public class RootController {
+	
+	@Autowired
+	Environment env;
 	
 	@GetMapping("/ping")
 	public String ping() {
-		return "Ping success - the root conroller is working";
+		System.out.println(env.getProperty("jam.prop.1"));
+		return "Ping success - the root conroller is working ::" + env.getProperty("jam.test.prop", String.class);
 	}
 
 }
